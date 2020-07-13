cf979b3732b5b5771a15f232ca6fea22c4702b61
Edward Joseph Valladares Tavera <evalladarestav@gmail.com>
2020-07-13 17:31:39 -0500

    Add page CAS
    Se agrega la primera parte de la pagina de Convocatoria CAS

---
+++ pages/gestion/conv_cas.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+
+		<?php
+		include '../constant/head-web.php';
+		?>
+		
+	<body>
+        <div class="body">
+			<?php
+    			include '../constant/header-web.php';
+            ?>
+            <div role="main" class="main">
+            <div class="container py-2">
+                    <div class="row">
+                        <div class="col">
+                            <hr class="solid my-5">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+
+                            <div class="tabs tabs-bottom tabs-center tabs-simple">
+                                <ul class="nav nav-tabs">
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="#tabsNavigationSimple1" data-toggle="tab">Directiva laboral</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#tabsNavigationSimple2" data-toggle="tab">Convocatorias</a>
+                                    </li>
+                                </ul>
+                                <div class="tab-content">
+                                    <div class="tab-pane active" id="tabsNavigationSimple1">
+                                        <div class="text-center">                                        
+                                            <div class="row mt-3">
+                                                <div class="col">              
+                                                    <div class="row">
+                                                        <div class="col-lg-4">
+                                                            <div class="tabs tabs-vertical tabs-right tabs-navigation tabs-navigation-simple">
+                                                                <ul class="nav nav-tabs col-sm-3">
+                                                                    <li class="nav-item active">
+                                                                        <a class="nav-link" href="#tabsNavigationVertSimple1" data-toggle="tab">Directiva Laboral</a>
+                                                                    </li>
+                                                                    <li class="nav-item">
+                                                                        <a class="nav-link" href="#tabsNavigationVertSimple2" data-toggle="tab">Proceso de Inscripción</a>
+                                                                    </li>
+                                                                    <li class="nav-item">
+                                                                        <a class="nav-link" href="#tabsNavigationVertSimple3" data-toggle="tab">Formato para la Inscripción</a>
+                                                                    </li>                                                                      
+                                                                </ul>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-lg-8">
+                                                            <div class="tab-pane tab-pane-navigation active mt-4" style="text-align:justify" id="tabsNavigationVertSimple1">                                            
+                                                                <h5>DIRECTIVA LABORAL</h5>
+                                                                <p>Los postulantes deben inscribirse en el lugar indicado en la convocatoria, adjuntando lo siguiente:</p>
+                                                                <p>&nbsp;&nbsp;&nbsp;a. Los formatos 1,2,3,A,B,C,D,E,F,G,H debidamente llenados (firmados con huella digital), que se descargan de la presente.</p>
+                                                                <ul>
+                                                                    <li>
+                                                                        Formato 1 - Solicitud de Postulante al Proceso de Selección.&nbsp;<br>
+                                                                    </li>
+                                                                    <li>
+                                                                        Formato 2 - Formato de Elección del Sistema Pensionario.<br>
+                                                                    </li>
+                                                                    <li>
+                                                                        Formato 3 - Formato de Ficha de Registro de Datos Personales.<br>
+                                                                    </li>
+                                                                    <li>
+                                                                        Formato A –&nbsp;Declaración Jurada Sobre vinculación con Algún(os) Funcionarios(S) o Servidores(ES) de la Municipalidad de villa el salvador.<br>
+                                                                    </li>
+                                                                    <li>
+                                                                        Formato B – Declaración Jurada de no Poseer Antecedentes Penales Ni Policiales.<br>
+                                                                    </li>
+                                                                    <li>
+                                                                        Formato C –&nbsp;Declaración jurada Información de Haber Prestado Servicios en Alguna Entidad de la Administración Pública.<br>
+                                                                    </li>
+                                                                    <li>
+                                                                        Formato D –&nbsp;Declaración Jurada Dominiciliaria.<br>
+                                                                    </li>
+                                                                    <li>
+                                                                        Formato E –&nbsp;De no contarse considerado como Deudor Alimentario Moroso.<br>
+                                                                    </li>
+                                                                    <li>
+                                                                        Formato F – Declaración Jurada Principio de Presunción de Veracidad contemplado en el Numeral.<br>
+                                                                    </li>
+                                                                    <li>
+                                                                        Formato G – Declaración Jurada de Conformidad al Literal(LEy de Seguridad y Salud en el Trabajo)<br>
+                                                                    </li>
+                                                                    <li>
+                                                                        Formato H – Declaración Jurada sobre veracidad de información proporcionada en el curriculum vitae.<br><br><br>
+                                                                    </li>
+                                                                </ul>
+                                                                <p>&nbsp;&nbsp;&nbsp;b. Currículum Vitae documentado, detallando principalmente los aspectos referidos a formación, experiencia laboral y capacitación requerida para la prestación de los servicios indicados. . Copia simple de los documentos que demuestren la formación profesional o técnica, la experiencia laboral y la capacitación adquirida, conforme a lo solicitado en el Aviso de Convocatoria.</p>
+                                                                <p>&nbsp;&nbsp;&nbsp;c. Copia simple del Documento Nacional de Identidad (DNI) vigente.</p>
+                                                                <p>&nbsp;&nbsp;&nbsp;d. Numero de RUC activo.</p>
+                                                                <br>
+                                                                <p>Los postulantes no deben haber sido destituidos de la Administración Pública en los últimos cinco (05) años, lo que se informa en el Formato 6; aquellos que hubieran sido despedidos por falta grave de entidades privadas, deberán informarlo en su solicitud de inscripción.  Los postulantes deben tener disponibilidad inmediata para incorporarse y cumplir con los servicios específicos que se indiquen en el Aviso de Convocatoria.</p>
+                                                                <p>Los postulantes ganadores de un proceso de selección, están obligados a presentar los documentos orinales que sustentan la información indicada en el currículum vitae presentado al proceso, y en caso de no sustentar documentadamente la información proporcionada, se les descalificará por haber presentado información falsa, con las implicancias que ello conlleva.</p>
+                                                            </div>
+                                                            <div class="tab-pane tab-pane-navigation" id="tabsNavigationVertSimple2">                                        
+                                                                <div class="col pb-2">
+                                                                    <h4 class="mt-4">PROCESO DE INSCRIPCION</h4>
+                                                                    <br>
+                                                                    <p style="text-align:justify;">Los postulantes deben tener presente lo siguiente: </p>
+                                                                    <p style="text-align:justify;">La Contratación Administrativa de Servicios (CAS) es una modalidad diferente a la Contratación Laboral Directa (regida por el Decreto Legislativo 276 y el Decreto Legislativo 728). Antes de inscribirse para participar en un proceso de selección, los postulantes deben verificar que cumplen todos los requisitos establecidos en el Aviso de Convocatoria.</p>
+                                                                </div>
+                                                            </div>
+                                                            <div class="tab-pane tab-pane-navigation" id="tabsNavigationVertSimple3">                                        
+                                                                <div class="col pb-3">
+                                                                    <h4 class="pb-4 mt-4">FORMATO PARA LA INSCRIPCIÓN</h4>
+                                                                    <br>
+                                                                    <a href="#"><p>Descargar Formulario de Inscripción</p></a>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane" id="tabsNavigationSimple2">
+                                        <div class="text-center">
+                                            <h4>HTML5 / CSS3 / JS</h4>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc <a href="#">vehicula</a> lacinia.</p>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc <a href="#">vehicula</a> lacinia. Nibh adipiscing sit amet. In eu justo a felis faucibus ornare vel id metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula. Fusce eget metus lorem, ac viverra leo. Nullam convallis, arcu vel pellentesque sodales, nisi est varius diam, ac ultrices sem ante quis sem. Proin ultricies volutpat sapien, nec scelerisque ligula mollis lobortis.</p>
+                                        </div>
+                                    </div>                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            
+            <?php
+    			include '../constant/footer-web.php';
+    		?>
+        </div>
+		<?php
+    		include '../constant/scripts-web.php';
+    	?>
+	</body>
+</html>
+        
