5be2c5d42ee899c89fcdaca1bf72cf79978c1fc8
Edward Joseph Valladares Tavera <evalladarestav@gmail.com>
2020-07-16 12:24:47 -0500

    Add resources Convocatoria cas 2013
    Se agrega el apartado de la convocatoria cas 2013 con su respectivo recurso

---
+++ resources/gestion/convocatorias-cas/2013/resultados/abril/footer.php
@@ -0,0 +1,73 @@
+<html  xmlns:fb="http://ogp.me/ns/fb#" xmlns:og="http://ogp.me/ns#" dir="ltr" lang="es-PE">
+<link rel="stylesheet" href="css/styler.css" type="text/css" media="screen" />
+<div id="footer">
+	  <div id="footer-wrapper">
+			
+			<ul id="follow"><div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+			  <li><p>Encuentranos en:</p></li>
+				
+				<li><a href="https://www.facebook.com/MunicipalidadDeVillaElSalvador" target="_blank"><img src="img/facebook.png" alt="Facebook" /></a></li>
+				
+			  <li><a href="http://www.youtube.com/user/vesinfo?feature=mhee" target="_blank"><img src="img/youtube.png" alt="Facebook" /></a></li>
+             		 
+			</ul>		
+<div class="footer-cols">
+				<div>
+					<ul>
+                    <li id="pies"><a href="http://correo.munives.gob.pe " target="_blank" class="custom">Correo Corporativo..</a></li>
+                   <li id="pies"><a href="http://munives.gob.pe/boletin2012.html" onClick="munienlace(this.href,'console',310,650);return false;" scrolling="no">Boletin</a></li>
+					  <li id="pies" ><a href="plano_distrital/Plano_Distrital.jpg"   target="_blank" class="custom">Plano Distrital</a></li>						
+						<li id="pies"><a href="http://munives.gob.pe/i.php?go=http://muni.daleunenter.com/nota_de_prensa.php" target="_parent">Notas de prensa</a></li>                      <li id="pies"><a href="http://www.munives.gob.pe/procesocas.php" target="_blank">CAS</a></li>
+                    
+						
+				  </ul>
+				</div>
+				<div>
+					<ul>
+						
+                           <li id="pies"><a href="http://www.participacionciudadanaves.org" onClick="munienlace(this.href,'console',450,190);return false;" target="_blank">Oficina de Participación </br>&nbsp;&nbsp;de Ciudadana</a></li>
+					  <li id="pies"><a href="http://www.peru.gob.pe/transparencia/pep_transparencia_lista_planes.asp?id_entidad=10122&id_tema=40" target="_blank">Agenda</a></li>
+                        <li id="pies"><a href="http://www.munives.gob.pe/Cooperacion_Tecnica_Internacional.php" target="_blank"> Cooperaci&oacute;n T&eacute;cnica</br>&nbsp;&nbsp; Internacional</a></li>
+                      	
+                    						
+				  </ul>
+				</div>
+				<div>
+					<ul>
+						<li id="pies"><a href="http://www.munives.gob.pe/pagos.html" onClick="munienlace(this.href,'console',285,300);return false;" target="_blank">Pago Electrónico</a></li>
+					  
+                     <li id="pies"><a href="frmaccesopublico/form_infopublica.pdf" target="_blank">Informaci&oacute;n P&uacute;blica</a></li>
+				<li id="pies"><a href="http://www.munives.gob.pe/declaracion.php" target="_parent" class="custom">Declaracion Jurada </br>&nbsp;&nbsp;de Funcionarios</a></li>
+                                 <li id="pies"><a href="http://munives.gob.pe/contenidos/pdf/PROGRAMA-DE-LA-MUJER.pdf" target="_blank">Programa de la Mujer</a></li>
+          
+                            
+                                            
+				  </ul>
+				</div>
+				<div>
+					<ul>
+                  <li id="pies"><a href="http://munives.gob.pe/zonificacion.php" target="_blank">Zonificación Propuestas</a></li>
+                     <li id="pies"><a href="http://munives.gob.pe/seguridad_ciudadana.html" onClick="munienlace(this.href,'console',600,500);return false;" target="_blank">Seguridad Ciudadana</a></li>
+                     <li id="pies"><a href="http://www.munives.gob.pe/comunicados_2013.php" target="_blank" class="custom">Comunicados</a></li>
+                                
+                  <li id="pies"><a href="tradoc1.php" tabindex="_blank">Consulta Estado de</br>&nbsp; Documento y/o Expediente</a></li>	
+
+                         
+			</ul>	               
+	  </div>
+				<br />
+				<br /><br /><br /><br /><br /><br /><br /><br />
+				<br /><br /><br /><br /><br />
+				<br />
+				<p id="copyfooter" align="center">Municipalidad de Villa El Salvador. Sector 2, Grupo 15 S/N. Av. Cesar Vallejo con Av. Revolución. Lima - Perú<br />
+Central Telefónica : (51) (1) 319-2530. Fax: (51) (1) 287-1071. E-mail: webmaster@munives.gob.pe<br />
+Copyrigth  2003 - <?php echo date("Y"); ?>. MVES. Todos los derechos reservados. <!-- inicio codigo contador --></p><p align="center"><img style="border: 0px solid ; display: inline;" alt="contador de visitas" 
+	src="http://contador-de-visitas.com/hit.php?id=1453557&counter=24"><!-- fin codigo contador -->
+</p><br /></div></div></div>
